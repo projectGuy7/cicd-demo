c0172e81cf0923ba6a02dd47f3abc14aefb69f81
Bolatkhanov Azamat <bolathanovazamat047@gmail.com>
2026-04-14 10:06:22 +0500

    Reset back

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -7,7 +7,7 @@ import sys
 import os
 
 # Add src to path so we can import calculator
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'src'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../..', 'src'))
 from calculator import add, subtract, multiply, divide, power
 
 class TestAddition:
